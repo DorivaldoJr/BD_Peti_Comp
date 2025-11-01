ec8933527153d01575510fec2c16978be6c0dc47
DorivaldoJr <dorivaldojsr@gmail.com>
2025-10-31 21:57:19 -0300

    feat(service): Adiciona estrutura inicial de PessoaService

--- src/main/java/com/peticompvc/bancodehoras/service/PessoaService.java
+++ src/main/java/com/peticompvc/bancodehoras/service/PessoaService.java
@@ -10,18 +10,4 @@ import java.sql.Timestamp;
 @Service
 public class PessoaService {
 
-    @Autowired
-    PessoaRepository repository;
-
-    PessoaDTOResponse savePerson(PessoaDTO pessoa){
-
-        // transforma pessoa dto em pessoa(entidade)
-        if(pessoa == null){
-            throw new Exception("Peson cannot be null");
-        }
-        repository.save(pessoa); // salva a entidade
-
-        // transforma no dto de resposta e retorna
-        return  pessoa;
-    }
 }
